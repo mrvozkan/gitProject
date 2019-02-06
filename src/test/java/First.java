260e2afcffe6399e6523792a60acd2f8a13f5b1c
Merve Ozkan <tchrmerveozkan@gmail.com>
2019-02-06 13:20:21 -0600

    add method

--- src/test/java/First.java
+++ src/test/java/First.java
@@ -2,4 +2,8 @@ public class First {
     public static void main(String[] args) {
         System.out.println("I have a mock interview on Saturday");
     }
+
+    public static void myName(){
+        System.out.println("I am so excited");
+    }
 }
