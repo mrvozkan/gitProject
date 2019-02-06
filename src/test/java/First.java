dd070513b4e2c0485b7b82d353d12feb91e760a2
Merve Ozkan <tchrmerveozkan@gmail.com>
2019-02-06 13:23:29 -0600

    add latest code

--- src/test/java/First.java
+++ src/test/java/First.java
@@ -6,5 +6,8 @@ public class First {
     public static void myName(){
         System.out.println("I am so excited");
     }
+    public static void myLife(){
+        System.out.println("I need to learn how to use github");
+    }
 }
 
