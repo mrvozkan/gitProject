d8d0c0aa416ca80a5f8058954e98f291f60d142d
mrvozkan <45147197+mrvozkan@users.noreply.github.com>
2019-02-06 15:17:35 -0600

    Update First.java

--- src/test/java/First.java
+++ src/test/java/First.java
@@ -2,7 +2,7 @@ public class First {
     public static void main(String[] args) {
         System.out.println("I have a mock interview on Saturday");
     }
-
+    //life is good with cup of coffee
     public static void myName(){
         System.out.println("I am so excited");
     }
