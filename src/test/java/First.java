9baa87e66a42b47ccb502aaa982697888f353123
Merve Ozkan <tchrmerveozkan@gmail.com>
2019-02-06 15:24:49 -0600

    add comment

--- src/test/java/First.java
+++ src/test/java/First.java
@@ -2,17 +2,21 @@ public class First {
     public static void main(String[] args) {
         System.out.println("I have a mock interview on Saturday");
     }
-
-    public static void myName(){
+    //add comment here
+    public static void myName() {
         System.out.println("I am so excited");
     }
-    public static void myLife(){
+
+    public static void myLife() {
         System.out.println("I need to learn how to use github");
     }
-    public static void firstClass(){
+
+    public static void firstClass() {
         System.out.println("I am excited for new adventures");
     }
-    public static void secondClass(){
-        System.out.println("If there is a will,there is a way");
-}
 
+    public static void secondClass() {
+        System.out.println("If there is a will,there is a way");
+    }
+
+}
